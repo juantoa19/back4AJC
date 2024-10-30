22ae35f94d767ebe0661a267a2f6a58b70e593a0
ismael-vargas <iwz.vargas@yavirac.edu.ec>
2024-10-30 10:40:12 -0500

    30 clase resuelto con exito ismael

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,22 +1,21 @@
-import { DataSource } from "typeorm"
+import { ConfigService } from "src/config/config.service";
+import { DataSource } from "typeorm";
 
-export const databaseProvider=[
-    {
-        provide: "DATABASE_CONNECTION",
-        useFactory:()=>{
-            const DataSourse= new DataSource(
-                {
-                    type:'postgres',
-                    host:'localhost',
-                    port: 5432,
-                    username:'postgres',
-                    password: 'postgres',
-                    database: 'back_nest_angular_IV'
+export const databaseProvider = [
+  {
+    provide: 'DATABASE_CONNECTION',
+    inject: [ConfigService],
+    useFactory: (config: ConfigService) => {
+      const dataSource = new DataSource({
+        type: 'postgres',
+        host: config.get('HOST'),
+        port: +config.get('PORT_DB'),
+        username: config.get('USERNAME'),
+        password: config.get('PASSWORD'),
+        database: config.get('DATABASE'),
+      });
 
-                }
-            );
-
-            return DataSourse.initialize();
-        }
+      return dataSource.initialize();
     }
-]
+  }
+];
