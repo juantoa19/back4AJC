743003a7d0896948db34409e3cbf93121f038d26
juantoa19 <jcc.toaso@yavira.edu.ec>
2024-11-26 10:57:31 -0500

    backend16

--- src/modules/categoria/categoria.service.ts
+++ src/modules/categoria/categoria.service.ts
@@ -16,18 +16,18 @@ export class CategoriaService {
   }
 
   async findAll() {
-    return await this.categoriaRepository.find();
+    return await this.categoriaRepository.find({order:{ id:'asc'}});
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} categoria`;
+  async findOne(id: number) {
+    //return await This.categoriaRepository.findOne(id);
   }
 
-  update(id: number, updateCategoriaDto: UpdateCategoriaDto) {
-    return `This action updates a #${id} categoria`;
+  async update(id: number, updateCategoriaDto: UpdateCategoriaDto) {
+    return await this.categoriaRepository.update(id,updateCategoriaDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} categoria`;
+  async remove(id: number) {
+    return await this.categoriaRepository.delete(id);
   }
 }
